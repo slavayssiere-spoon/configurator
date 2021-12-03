fc7418ed2ba9a3ce17c78bb255f502ba7886d76c
slavayssiere <sebastien.lavayssiere@spoon.ai>
2021-12-03 11:02:22 +0100

    add golang lib

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ set -e
 python3 -m pip install grpcio
 python3 -m pip install grpcio-tools
 
-GEN_PATH="configurator"
-mkdir -p $GEN_PATH
-GOPATH=$(go env GOPATH)
+
+GEN_PATH="."
 GO_LIB_PATH=$(go env GOPATH)/src
+GOPATH=$(go env GOPATH)
 
 GROUPS_VERSION="master"
 curl -o proto/groups.proto "https://raw.githubusercontent.com/slavayssiere-spoon/groups/$GROUPS_VERSION/proto/groups.proto"
@@ -16,7 +16,29 @@ curl -o proto/groups.proto "https://raw.githubusercontent.com/slavayssiere-spoon
 ROBOTS_VERSION="master"
 curl -o proto/robots.proto "https://raw.githubusercontent.com/slavayssiere-spoon/robots/$ROBOTS_VERSION/proto/robots.proto"
 
-echo "launch protoc"
+echo "gen protoc golang"
+protoc \
+        -I proto \
+        -I $GO_LIB_PATH/include \
+        --go_out=$GEN_PATH      --go_opt=paths=source_relative \
+        --go-grpc_out=$GEN_PATH --go-grpc_opt=paths=source_relative \
+        --grpc-gateway_out=logtostderr=true,paths=source_relative:$GEN_PATH \
+        --openapiv2_out=logtostderr=true:$GEN_PATH \
+        proto/conf.proto
+
+protoc-go-inject-tag -input=./conf.pb.go
+
+echo "mod tidy"
+go mod tidy
+
+echo "update"
+go get -u ./...
+
+
+GEN_PATH="configurator"
+mkdir -p $GEN_PATH
+
+echo "launch protoc python"
 
 python3 -m grpc_tools.protoc \
         -I proto \
---
+++ svc-conf.go
@@ -0,0 +1,124 @@
+package authorizations
+
+import (
+	"context"
+	"time"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
+	grpc_opentracing "github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+	opentracing "github.com/opentracing/opentracing-go"
+
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+
+	lib "gitlab.com/SpoonQIR/Cloud/library/golang-common.git"
+)
+
+type ConfService struct {
+	Confconn *grpc.ClientConn
+	Confsvc  ConfiguratorClient
+	Confreco chan bool
+
+	Id *lib.Identity
+}
+
+// InitConf tst
+func (s *ConfService) InitConf(authorizationHost string, tracer opentracing.Tracer, logger *logrus.Logger) chan bool {
+	logentry := logrus.NewEntry(logger)
+	logopts := []grpc_logrus.Option{
+		grpc_logrus.WithDurationField(func(duration time.Duration) (key string, value interface{}) {
+			return "grpc.time_ns", duration.Nanoseconds()
+		}),
+	}
+
+	otopts := []grpc_opentracing.Option{
+		grpc_opentracing.WithTracer(tracer),
+	}
+
+	var err error
+
+	connect := make(chan bool)
+
+	go func(lconn chan bool) {
+		for {
+			logrus.Info("Wait for connect")
+			r := <-lconn
+			logrus.WithFields(logrus.Fields{"reconn": r}).Info("conn chan receive")
+			if r {
+				for i := 1; i < 5; i++ {
+					// s.authconn, s.Confsvc,
+					s.Confconn, err = grpc.Dial(authorizationHost,
+						grpc.WithInsecure(),
+						grpc.WithUnaryInterceptor(grpc_middleware.ChainUnaryClient(
+							grpc_logrus.UnaryClientInterceptor(logentry, logopts...),
+							grpc_opentracing.UnaryClientInterceptor(otopts...),
+							grpc_prometheus.UnaryClientInterceptor,
+						)),
+						grpc.WithStreamInterceptor(grpc_middleware.ChainStreamClient(
+							grpc_logrus.StreamClientInterceptor(logentry, logopts...),
+							grpc_opentracing.StreamClientInterceptor(otopts...),
+							grpc_prometheus.StreamClientInterceptor,
+						)),
+					)
+					if err != nil {
+						logger.Fatalf("did not connect: %v, try : %d - sleep 5s", err, i)
+						time.Sleep(2 * time.Second)
+					} else {
+						s.Confsvc = NewConfiguratorClient(s.Confconn)
+						break
+					}
+				}
+			} else {
+				logrus.Info("end of goroutine - reconnect")
+				return
+			}
+		}
+	}(connect)
+
+	logger.WithFields(logrus.Fields{"host": authorizationHost}).Info("Connexion au service gRPC 'Conf'")
+	connect <- true
+	return connect
+}
+
+func (s *ConfService) GetConf(ctx context.Context, usr *Conf) (*Conf, error) {
+	for i := 1; i <= 5; i++ {
+		md, ok := metadata.FromOutgoingContext(ctx)
+		if !ok {
+			logrus.Error("cannot get outgoing data")
+		}
+		logrus.WithFields(logrus.Fields{"usr": usr, "jwt": md.Get("authorization")}).Debug("get authorizations")
+		grp, err := s.Confsvc.Get(metadata.NewOutgoingContext(ctx, md), usr)
+		logrus.WithFields(logrus.Fields{"ctx.err": ctx.Err(), "err": err}).Trace("error ctx get object")
+		if err != nil {
+			logrus.WithFields(logrus.Fields{"err": err}).Error("error get object")
+			errStatus, _ := status.FromError(err)
+			if errStatus.Code() == codes.Unavailable {
+				s.Confreco <- true
+			} else if errStatus.Code() == codes.Canceled {
+				s.Confreco <- true
+			} else if errStatus.Code() == codes.DeadlineExceeded {
+				s.Confreco <- true
+			} else if errStatus.Code() == codes.Aborted {
+				s.Confreco <- true
+			} else if errStatus.Code() == codes.Unauthenticated {
+				logrus.WithFields(logrus.Fields{"jwt": md.Get("authorization")}).Info("ws-identity not identified")
+				return nil, status.Error(codes.Unauthenticated, "unauthenticated")
+			} else if errStatus.Code() == codes.InvalidArgument {
+				return nil, status.Errorf(codes.InvalidArgument, "argument invalid %v", err)
+			} else if errStatus.Code() == codes.NotFound {
+				return nil, nil
+			}
+			// errStatus.Code() == codes.Internal = retry
+		} else if ctx.Err() != nil {
+			s.Confreco <- true
+		} else {
+			return grp, nil
+		}
+	}
+	return nil, status.Errorf(codes.NotFound, "authorization not found")
+}
